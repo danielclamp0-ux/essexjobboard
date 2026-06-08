e563916898223463376e1e15a32ed51b52f99beb
danielclamp0-ux <danielclamp360@gmail.com>
2026-06-08 22:27:13 +0100

    Update jobs.js

--- api/jobs.js
+++ api/jobs.js
@@ -2,6 +2,9 @@ module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
 
+  // Cache responses for 30 minutes at the edge
+  res.setHeader('Cache-Control', 's-maxage=1800, stale-while-revalidate=3600');
+
   const { where, category, page, what } = req.query;
 
   const ADZUNA_ID = process.env.ADZUNA_ID;
@@ -13,7 +16,7 @@ module.exports = async function handler(req, res) {
 
   try {
     const pageNum = page || 1;
-    let url = `https://api.adzuna.com/v1/api/jobs/gb/search/${pageNum}?app_id=${ADZUNA_ID}&app_key=${ADZUNA_KEY}&results_per_page=20&content-type=application/json`;
+    let url = `https://api.adzuna.com/v1/api/jobs/gb/search/${pageNum}?app_id=${ADZUNA_ID}&app_key=${ADZUNA_KEY}&results_per_page=20&content-type=application/json&days_old=90&sort_by=date`;
     if (where) url += `&where=${encodeURIComponent(where)}`;
     if (category) url += `&category=${encodeURIComponent(category)}`;
     if (what) url += `&what=${encodeURIComponent(what)}`;

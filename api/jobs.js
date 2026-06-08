68685ad81dd83c6f23ef53f484e463b42a2e31a1
danielclamp0-ux <danielclamp360@gmail.com>
2026-06-08 22:31:02 +0100

    jobs.js

--- api/jobs.js
+++ api/jobs.js
@@ -2,9 +2,6 @@ module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
 
-  // Cache responses for 30 minutes at the edge
-  res.setHeader('Cache-Control', 's-maxage=1800, stale-while-revalidate=3600');
-
   const { where, category, page, what } = req.query;
 
   const ADZUNA_ID = process.env.ADZUNA_ID;
